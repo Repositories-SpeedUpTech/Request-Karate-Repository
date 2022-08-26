5905225f4c2ba1c59d100aec728ad8c461eda659
Ismael Carvajal <ismaelcarvajal@outlook.com>
2022-08-26 11:39:16 -0500

    Modify LoginUser.java file

--- Request-Karate-Repository/src/test/java/karate/request/login/LoginUser.java
+++ Request-Karate-Repository/src/test/java/karate/request/login/LoginUser.java
@@ -3,7 +3,7 @@ package karate.request.login;
 import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit5.Karate;
 
-@KarateOptions(features = "classpath:karate/request/create.feature", tags = {"@ignoreScenario", "@ignoreScenarioOutline"})
+@KarateOptions(features = "classpath:karate/request/Login/2. LoginUser/loginUser.feature", tags = {"@LoginUser"})
 public class LoginUser {
 
     @Karate.Test
